71f5719dd6675aeefd651bdff60897b757ef1adf
Jason Scott <jason@jaylin.com.au>
2017-11-21 11:42:26 +0800

    add fiori object header and simple property cells to the detail screen

--- Customer/DetailViewController.swift
+++ Customer/DetailViewController.swift
@@ -13,7 +13,7 @@ import SAPFiori
 import SAPCommon
 
 
-class DetailViewController: UIViewController {
+class DetailViewController: FUIFormTableViewController {
 
     private let appDelegate = UIApplication.shared.delegate as! AppDelegate
     private let logger: Logger = Logger.shared(named: "DetailViewController")
@@ -25,12 +25,36 @@ class DetailViewController: UIViewController {
         }
     }
     
+    var objectHeader: FUIObjectHeader!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Create Fiori Object Header
+        tableView.register(FUIObjectTableViewCell.self, forCellReuseIdentifier: FUIObjectTableViewCell.reuseIdentifier)
         
+        objectHeader = FUIObjectHeader()
+        
+        tableView.tableHeaderView = objectHeader
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.separatorStyle = .none
+        
+        if let objectHeader = tableView.tableHeaderView as? FUIObjectHeader {
+            objectHeader.headlineText = "\(_entity.firstName!) \(_entity.lastName!)"
+            objectHeader.subheadlineText = _entity.customerID
+            objectHeader.tags = [FUITag(title: "Started"), FUITag(title:"PM01"), FUITag(title:"103-Repair")]
+            objectHeader.footnoteText = "\(_entity.city!), \(_entity.country!)"
+            objectHeader.descriptionText = "This is the description text..."
+            objectHeader.statusText = "High"
+            objectHeader.substatusImage = #imageLiteral(resourceName: "person-placeholder")
+        }
+        
+        // Create Fiori Simple Property
+        tableView.register(FUISimplePropertyFormCell.self, forCellReuseIdentifier: FUISimplePropertyFormCell.reuseIdentifier)
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.separatorStyle = .none
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,7 +62,42 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    /**
+     Specify the number of rows in the table collection. Here we return a row for the Fiori
+     Object Header and additional rows for each Fiori Simple Property Cell.
+    */
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            return UITableViewCell( )
+        } else {
+            let simplePropertyCell = tableView.dequeueReusableCell(withIdentifier: FUISimplePropertyFormCell.reuseIdentifier, for: indexPath) as! FUISimplePropertyFormCell
+            
+            switch indexPath.row {
+            case 1:
+                simplePropertyCell.keyName = "E-mail"
+                simplePropertyCell.value = _entity.emailAddress!
+                simplePropertyCell.keyLabel.font = UIFont.preferredFioriFont(forTextStyle: .body)
+                simplePropertyCell.valueTextField.font = UIFont.preferredFioriFont(forTextStyle: .body)
+            case 2:
+                simplePropertyCell.keyName = "Phone Number"
+                simplePropertyCell.value = _entity.phoneNumber!
+                simplePropertyCell.keyLabel.font = UIFont.preferredFioriFont(forTextStyle: .body)
+                simplePropertyCell.valueTextField.font = UIFont.preferredFioriFont(forTextStyle: .body)
+            default:
+                simplePropertyCell.keyName = "Address"
+                simplePropertyCell.value = "\(_entity.houseNumber!) \(_entity.street!), \(_entity.city!) \(_entity.postalCode!)"
+                simplePropertyCell.keyLabel.font = UIFont.preferredFioriFont(forTextStyle: .body)
+                simplePropertyCell.valueTextField.font = UIFont.preferredFioriFont(forTextStyle: .body)
+            }
+            
+            return simplePropertyCell
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
